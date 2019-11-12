c780b1e87d163c1452f4a61833a444bb05008ec2
arseniy <molodeckij.b.2017@fml31.ru>
2019-11-12 23:43:48 +0300

    updated code to make it more parallel

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # parallel-gradient-descent
+
+# BUILD
+use command
+```
+mvn package
+```
+
+# DEPLOY
+use command
+
+```
+spark-submit --class App --master local[2] target/ParallelGradientDescent-1.0-SNAPSHOT.jar
+```
+where 2 is number of workers
--- src/main/java/ParallelGD.java
+++ src/main/java/ParallelGD.java
@@ -11,7 +11,35 @@ import scala.Tuple2;
 import java.util.Arrays;
 
 public class ParallelGD {
-    public void makegd(String path){
+    private double maxErrorValue;
+    private JavaRDD<LabeledPoint> data;
+    private LinearRegressionModel model;
+    private JavaSparkContext sparkContext;
 
+    public ParallelGD(double MaxErrorValue, JavaSparkContext context){
+        maxErrorValue = MaxErrorValue;
+        sparkContext = context;
     }
+    public LinearRegressionModel Train(JavaRDD<LabeledPoint> inputData){
+        data = sparkContext.parallelize(inputData.collect());
+        model = LinearRegressionWithSGD.train(data.rdd(),1);
+        while (!isModelCompleted()){
+            data.mapPartitions(a -> {
+                return a;
+            });
+        }
+        return model;
+    }
+
+    private boolean isModelCompleted(){
+        JavaRDD<Tuple2<Double, Double>> valuesAndPred = data
+                .map(point -> new Tuple2<>(
+                        point.label(),
+                        model.predict(point.features())));
+
+        double MSE = valuesAndPred.mapToDouble(
+                tuple -> Math.pow(tuple._1 - tuple._2, 2)).mean();
+        return MSE > maxErrorValue;
+    }
+
 }
