65bfd6677f62291eaa4e56e7f91a1350c484db28
arseniy <molodeckij.b.2017@fml31.ru>
2019-11-15 02:24:36 +0300

    Added separated class for generating random model

---
+++ src/main/java/RandomModelGenerator.java
@@ -0,0 +1,15 @@
+import org.apache.spark.mllib.linalg.Vectors;
+import org.apache.spark.mllib.regression.LinearRegressionModel;
+
+import java.util.Random;
+
+public class RandomModelGenerator {
+    public LinearRegressionModel generate(int weightCount){
+        Random r = new Random();
+        double[] randomWeights = new double[weightCount];
+        for (int i = 0; i < weightCount; i++) {
+            randomWeights[i] = r.nextDouble();
+        }
+        return new LinearRegressionModel(Vectors.dense(randomWeights), r.nextDouble());
+    }
+}
