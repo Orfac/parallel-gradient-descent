d1d180576fff7e18145c764bbd06f7b0aa53b586
arseniy <molodeckij.b.2017@fml31.ru>
2019-11-20 06:48:39 +0300

    Updated checking algorithm to work until difference will be low

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,25 +19,24 @@ import java.util.Random;
 
 public class App {
         static String path = "/home/arseniy/dev/learn_projects/math/ParallelGradientDescent/lpsa.data";
-        static double possibleDifference = 1e-15;
+        static double possibleDifference = 0.0001;
 
     public static void main(String[] args) {
 
         SparkConf conf = new SparkConf().setAppName("ParallelGD");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
-        JavaRDD<String>[] input = sparkContext.textFile(path).randomSplit(new double[]{0.7, 0.3});
+        JavaRDD<String> input = sparkContext.textFile(path);
 
         InputParser parser = new InputParser();
-        JavaRDD<LabeledPoint> trainData = parser.parse(input[0]);
-        JavaRDD<LabeledPoint> testData = parser.parse(input[1]);
+        JavaRDD<LabeledPoint> data = parser.parse(input);
 
         ParallelGD pgd = new ParallelGD(sparkContext);
-        LinearRegressionModel model = pgd.Train(trainData);
+        LinearRegressionModel model = pgd.Train(data);
 
         ErrorCalculator calculator = new ErrorCalculator();
 
-        double mse = calculator.MeanSquaredError(model,testData);
+        double mse = calculator.MeanSquaredError(model,data);
 
         // Count amount of operations for learning
         int counter = 1;
@@ -45,15 +44,15 @@ public class App {
         do {
             oldMse = mse;
 
-            model = pgd.Train(trainData,model);
-            mse = calculator.MeanSquaredError(model,testData);
+            model = pgd.Train(data,model);
+            mse = calculator.MeanSquaredError(model,data);
 
             System.out.println(counter + " " + mse);
             counter++;
         } while (Math.abs(mse - oldMse) > possibleDifference);
 
 
-        System.out.println(counter);
+        System.out.println(counter + " " +  Arrays.toString(model.weights().toArray()));
         sparkContext.stop();
 
     }
